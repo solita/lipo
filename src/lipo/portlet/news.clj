e0677a778d2c5bd6b24ea25b328fb19919e0d355
jkorpelin <joni.korpelin@solita.fi>
2021-09-08 16:27:04 +0300

    Fixes news portlet query and adds invalid params component to be shown on error

--- src/lipo/portlet/news.clj
+++ src/lipo/portlet/news.clj
@@ -2,7 +2,11 @@
   "News portlet lists the latest content in a path (defaults to here)."
   (:require [lipo.portlet :as p]
             [ripley.html :as h]
-            [lipo.db :as db]))
+            [lipo.db :as db]
+            [lipo.content-db :as content-db]
+            [lipo.content-model :as content-model]
+            [clojure.spec.alpha :as s]
+            [clojure.set :as set]))
 
 (defn- render-item [_ctx {:content/keys [title excerpt]
                           at :meta/at
@@ -16,24 +20,71 @@
       [:div.at date-time]
       [:div.excerpt excerpt]])))
 
+(comment
+  '{:find [?anc ?parent]
+    :where [(ancestor ?content ?anc)
+            [?anc :content/parent ?parent]]
+    :in [?content]
+    :rules [[(ancestor ?c ?a)
+             [(== ?c ?a)]]
+            [(ancestor ?c ?a)
+             [?c :content/parent ?a]]
+            [(ancestor ?c ?a)
+             [?c :content/parent ?p]
+             (ancestor ?p ?a)]]})
+
+(defn valid-params?
+  [{:keys [types path max-items]}]
+  ;; check that type is collection of keywords
+  (and
+    (or (nil? types) (set/subset? (set types) content-model/content-types))
+    (or (nil? max-items) (int? max-items))
+    (or (nil? path) (re-matches content-model/uri-path-pattern path))))
+
+(defn news-portlet-invalid-params
+  [params]
+  (h/html
+    [:div.border-2.border-red-500.p-5
+     [:<>
+      [:p "Virheellinen portlet määritelmä: "
+       (h/dyn!
+         (str params))]
+      [:p "Esimerkki oikeasta määritelmästä: "
+       (h/dyn! (pr-str {:portlet/type :news :types [:memo :news] :max-items 40 :path "/polku/toinen"}))]
+      [:p "Validit tyypit: "
+       (h/dyn! (pr-str content-model/content-types))]]]))
+
 (defmethod p/render :news [{:keys [here db] :as ctx}
-                           {:keys [type path max-items]
-                            :or {type :news
-                                 max-items 20}}]
-  (let [items (map first
-                   (db/q db
-                         (merge
-                          {:limit max-items}
-                          '{:find [(pull ?item [:crux.db/id
-                                                :content/title
-                                                :content/excerpt
-                                                :meta/at]) ?at]
-                            :where [[?item :content/type ?type]
-                                    [?item :meta/at ?at]]
-                            :order-by [[?at :desc]]
-                            :in [?type]})
-                         ;; FIXME: check rule that  path is ancestor of item
-                         type max-items))]
-    (h/html
-     [::h/for [item items]
-      (render-item ctx item)])))
+                           {:keys [types path max-items]
+                            :or {types content-model/content-types
+                                 max-items 20
+                                 path content-model/root-page-id}
+                            :as params}]
+  (if-not (valid-params? params)
+    (news-portlet-invalid-params params)
+    (let [entity-of-path (content-db/content-id db path)
+
+          items (map first
+                  (db/q db
+                    (merge
+                      {:limit max-items}
+                      '{:find [(pull ?item [:crux.db/id
+                                            :content/title
+                                            :content/excerpt
+                                            :content/parent]) ?created]
+                        :where
+                        [[?item :content/type ?types]
+                         [?item :meta/created ?created]
+                         (ancestor ?item ?ancestor)]
+                        :rules [[(ancestor ?c ?a)
+                                 [?c :content/parent ?a]]
+                                [(ancestor ?c ?a)
+                                 [?c :content/parent ?p]
+                                 (ancestor ?p ?a)]]
+                        :order-by [[?created :desc]]
+                        :in [[?types ...] ?ancestor]})
+                    ;; FIXME: check rule that  path is ancestor of item
+                    types entity-of-path))]
+      (h/html
+        [::h/for [item items]
+         (render-item ctx item)]))))
