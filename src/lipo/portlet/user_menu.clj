8fa4b79ab30b7c1f225bf06b7473efab7537cfc0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-09-08 09:34:27 +0300

    Show gravatar and initials

--- src/lipo/portlet/user_menu.clj
+++ src/lipo/portlet/user_menu.clj
@@ -2,14 +2,26 @@
   "User menu portlet. Shows currently logged in user, or link to login."
   (:require [lipo.portlet :as p]
             [re-html-template.core :refer [html]]
-            [ripley.html :as h]))
+            [clojure.string :as str]))
+
+(defn- md5 [s]
+  (str/join
+   (map #(format "%x" %)
+        (.digest
+         (doto (java.security.MessageDigest/getInstance "MD5")
+           (.update (.getBytes s "UTF-8")))))))
 
 (defmethod p/render :user-menu [{{user :user} :request :as ctx} _]
   (def *u user)
-  (if-let [user-name (when (seq user)
-                       (str (:user/given-name user) " " (:user/family-name user)))]
-    (html {:file "templates/user-menu.html" :selector "div.logged-in"}
-          :span.user-name {:replace-children user-name})
-    (let [login-url (get-in ctx [:auth :login-url] "FOOBAR")]
+  (if (seq user)
+    (let [hash (or (some->> user :user/email str/lower-case md5) "")
+          gravatar-url (format "https://www.gravatar.com/avatar/%s?size=32&d=mp" hash)
+          initials (str/upper-case
+                    (str (some-> user :user/given-name first)
+                         (some-> user :user/family-name first)))]
+      (html {:file "templates/user-menu.html" :selector "div.logged-in"}
+            :img.gravatar {:set-attributes {:src gravatar-url}}
+            :span.user-initials {:replace-children initials}))
+    (let [login-url (get-in ctx [:auth :login-url])]
       (html {:file "templates/user-menu.html" :selector "div.unauthenticated"}
             :a {:set-attributes {:href login-url}}))))
