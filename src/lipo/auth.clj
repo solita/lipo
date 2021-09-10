0d7a40f47fadb127bd00e149f6f53dd2559ad9da
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-09-10 10:25:56 +0300

    Add logout and logged-out routes

--- src/lipo/auth.clj
+++ src/lipo/auth.clj
@@ -2,7 +2,12 @@
   "OAuth2 and JWT token user auth."
   (:require [ring.middleware.oauth2 :as oauth2]
             [ring.middleware.token :as token]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [compojure.core :refer [routes GET]]
+            [re-html-template.core :refer [html]]
+            [ring.util.io :as ring-io]
+            [clojure.java.io :as io]
+            ripley.impl.output))
 
 (defn- request-jwt-token [req]
   (some-> req
@@ -45,6 +50,32 @@
           :can-edit? (:can-edit? auth some?) ; by default authenticated user can edit
           :login-url (some-> config :auth :oauth2 vals first :launch-uri)}))
 
+(defn- logged-out-url [{:keys [scheme headers]}]
+  (str (name scheme) "://" (get headers "host") "/logged-out"))
+
+(defn- logged-out-page []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (ring-io/piped-input-stream
+          (fn [out]
+            (with-open [w (io/writer out)]
+              (binding [ripley.impl.output/*html-out* w]
+                (html {:file "templates/logged-out.html" :selector "html"})))))})
+
+(defn wrap-logout-paths [handler {:keys [logout-uri]}]
+  (routes
+   (GET "/logout" req
+        {:status 302
+         :cookies {"ring-session" {:value "", :max-age 1}}
+         :headers {"Location" (str logout-uri
+                                   (java.net.URLEncoder/encode
+                                    (logged-out-url req)))}
+         :session nil})
+   (GET "/logged-out" _req
+        (logged-out-page))
+
+   handler))
+
 (defn wrap-auth [handler {auth :auth :as _config}]
   (if (and (contains? auth :jwt)
            (contains? auth :oauth2))
@@ -53,7 +84,8 @@
          (merge (:jwt auth)
                 {:claims->user (or (:claims->user auth) identity)}))
         (oauth2/wrap-oauth2 (:oauth2 auth))
-        (wrap-force-request-scheme (:force-request-scheme auth)))
+        (wrap-force-request-scheme (:force-request-scheme auth))
+        (wrap-logout-paths auth))
     (do
       (log/warn "No OAuth2 and JWT configuration.")
       handler)))
