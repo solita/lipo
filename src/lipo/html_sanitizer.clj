8422ef19eb27252ae7cd8f2f47ad882dcb02224c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-09-15 15:00:42 +0300

    Relax sanitizer text content

--- src/lipo/html_sanitizer.clj
+++ src/lipo/html_sanitizer.clj
@@ -2,7 +2,8 @@
   "Use OWASP HTML sanitizer to cleanup user input of potentially
   dangerous content."
   (:import (org.owasp.html HtmlPolicyBuilder
-                           Sanitizers)))
+                           Sanitizers))
+  (:require [clojure.string :as str]))
 
 (def policy (reduce #(.and %1 %2)
                     [Sanitizers/BLOCKS
@@ -12,5 +13,16 @@
                      Sanitizers/STYLES
                      Sanitizers/TABLES]))
 
+(defn- sanitize-with-policy [html]
+  (.sanitize policy html))
+
 (defn sanitize [input-html]
-  (.sanitize policy input-html))
+  (-> input-html
+      sanitize-with-policy
+
+      ;; Sanitizer is too strict, we DO want to allow {{...edn...}} in content
+      ;; that is read in as maps.
+      (str/replace "{<!-- -->{" "{{" )
+
+      ;; also allow real doublequote
+      (str/replace "&#34;" "\"")))
